bd442845593ae31f1600ef6635a7888f3bac7585
gongchen322 <gong27@purdue.edu>
2016-01-22 01:20:02 -0800

    Major Update
    Add Twitter Collection Feature.
    Add Home Page design
    Update CSS
    Removed React-dom bug
    updated package.json

--- app/js/components/home_component/home.js
+++ app/js/components/home_component/home.js
@@ -1,6 +1,6 @@
 'use strict';
 import React from 'react';
-
+import Router from 'react-router';
 
 var img =
 ['../assets/images/bg3.jpg',
@@ -23,11 +23,12 @@ class Home extends React.Component {
     return (
       <div>
         <div id="axis" className="one ">
+          <img className="object click" src="../assets/images/Twitter/click.png"/>
           <img className="object dialog" src="../assets/images/Twitter/dialog.png"/>
           <img className="object laba" src="../assets/images/Twitter/laba.jpg"/>
           <img className="object van move-right" src="../assets/images/van-to-right.png"/>
           <img className="object folder" src="../assets/images/Twitter/folder.png"/>
-          <img className="object arrow" src="../assets/images/Twitter/arrow.jpeg"/>
+          <Router.Link to="main"><img className="object arrow" src="../assets/images/Twitter/arrow.png"/></Router.Link>
         </div>
       </div>
     );
--- app/js/components/home_component/routes.js
+++ app/js/components/home_component/routes.js
@@ -7,15 +7,15 @@ import Go from './go';
 import One from './one';
 import Two from './two';
 import Three from './three';
-import Four from './four';
+import Application from '../twitter_component/Application.react';
 
 const routes = (
   <Route handler={Layout}>
     <DefaultRoute name="home" handler={Home}/>
-    <Route name="go" path="/go" handler= {Go}/>
+    <Route name="1" path="/1" handler= {One}/>
     <Route name="2" path="/2" handler={Two}/>
     <Route name="3" path="/3" handler={Three}/>
-     <Route name="4" path="/4" handler={Four}/>
+     <Route name="main" path="/main" handler={Application}/>
   </Route>
 );
 
